6d48d7ec3f3b8ce35d9513a63b0802b36fb81976
Sam W <11swortzman11@gmail.com>
2024-07-10 14:14:14 -0700

    oops

---
+++ src/routes/content/[[path]]/[page]/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let data;
+</script>
+
+<div class="md-content container p-20">
+    <svelte:component this={data.content} />
+</div>
---
+++ src/routes/content/[[path]]/[page]/+page.ts
@@ -0,0 +1,18 @@
+
+import { error } from "@sveltejs/kit";
+
+export async function load({ params }) {
+    
+    try {
+		console.log(params.page, "dskujfhkjdfs");
+		const post = await import(`/src/content/${params.path != null ? params.path + '/' : ''}${params.page}.md`);
+		console.log("hi", params);
+        return {
+			content: post.default,
+			meta: post.metadata
+		}
+
+	} catch (e) {
+		throw error(404, `Could not find ${JSON.stringify(params)}`)
+	}
+}
